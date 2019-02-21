1152e46bad00a614c0c21d561dd42afd00c9e63b
Jenna Surace <44848974+Jsurace@users.noreply.github.com>
2019-02-21 08:51:47 -0500

    Add files via upload

--- SortArray.java
+++ SortArray.java
@@ -1,34 +1,33 @@
 /*
 Filename:	SortArray.java
 Author:	Jenna
-Date:	2/18/19
+Date:	2/21/19
 */
 
 import java.util.*;
+import java.util.Arrays;
 
 public class SortArray
 {
 	public static void main(String[] args)
 	{
-		int[] numArray = {22, 100, 9, 35, 40, 2, 88, 17, 55, 60};
-		int x, y, temp;
-		x = 1;
 
-		System.out.println("Array is as follows before sorting: " + numArray);
-/*
-		while(x < numArray.length)
-		{
-			temp = numArray[x];
-			y = x - 1;
-			while(y >= 0 && numArray[y] > temp)
-			{
-				numArray[y + 1] = numArray[y];
-				--y;
-			}
-			numArray[y + 1] = temp;
-			++x;
-		} */
-		System.out.println("Sorting Complete: " + numArray[x]);
+	int temp, num;
+	String numString;
+	int[] numArray = {22, 100, 9, 35, 40, 2, 88, 17, 55, 60};
+
+	for (int y = 0; y < numArray.length; y++) {
+	    for (int x = y; x < numArray.length; x++) {
+	        if (numArray[y] < numArray[x]) {
+	            temp = numArray[y];
+	            numArray[y] = numArray[x];
+	            numArray[x] = temp;
+	        }
+	    }
+	}
+
+	numString = Arrays.toString(numArray);
+
+	System.out.println(numString);
 	}
 }
-
